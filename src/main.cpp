f0e3838124ef257532db554bca26a23d2adcdefb
Gleb Frolov <60653769+GlebFrolov-programmer@users.noreply.github.com>
2024-04-06 12:57:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* cstr = "It is a very smart function!";
+    std::cout << "Function faStr1: " << faStr1(cstr) << std::endl;
+    std::cout << "Function faStr2: " << faStr2(cstr) << std::endl;
+    std::cout << "Function faStr3: " << faStr3(cstr) << std::endl;
     return 0;
 }
